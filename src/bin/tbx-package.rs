ce542d3c2c0696dbf7198eecb8ae8380caf42533
Samwns <samngueds@gmail.com>
2026-08-02 12:31:15 -0300

    Make Cargo packaging aliases executable

---
+++ src/bin/tbx-package.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::{Command, ExitCode};
+
+fn main() -> ExitCode {
+    let target = env::args().nth(1).unwrap_or_else(|| "all".into());
+    let script = match target.as_str() {
+        "linux" => "scripts/package-linux.sh",
+        "windows" => "scripts/package-windows.sh",
+        "all" => "scripts/package-all.sh",
+        _ => {
+            eprintln!("Uso: cargo package-[all|linux|windows]");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    match Command::new("bash").arg(root.join(script)).status() {
+        Ok(status) if status.success() => ExitCode::SUCCESS,
+        Ok(status) => ExitCode::from(status.code().unwrap_or(1) as u8),
+        Err(error) => {
+            eprintln!("Não foi possível executar o script de release: {error}");
+            ExitCode::FAILURE
+        }
+    }
+}
